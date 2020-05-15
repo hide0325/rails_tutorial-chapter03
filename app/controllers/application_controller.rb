a1ce0b209db3b4cd316d907632b60aab6da2acd0
hide0325 <tmnftc@yahoo.co.jp>
2020-05-15 01:56:49 +0000

    Implement basic login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  include SessionsHelper
 
-  def hello
-    render html: "hello, world!"
+  # 渡されたユーザーでログインする
+  def log_in(user)
+    session[:user_id] = user.id
   end
 end
